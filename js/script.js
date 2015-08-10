3ac9832d39e3e1ef80732cf065cc659bc5af4ce9
matthewgaim <matthewgaim@gmail.com>
2015-08-09 18:55:13 -0700

    Stuff
    Added Picture, Small Description, Background & some disfunctional server
    stuff

---
+++ js/script.js
@@ -0,0 +1,12 @@
+debugger
+function hackathons(){
+  alert("AngelHack 8 SF, Silicon Valley and Teen Hackathon");
+}
+var time = "Hi";//, seconds, hours, theTime;
+/**while(true){
+    time = new Date();
+    seconds = time.getSeconds();
+    hours = time.getHours();
+    theTime = hours.toString() + ":" + seconds.toString();**/
+    document.getElementById("time").innerHTML = "The time is " + time ;
+//}
