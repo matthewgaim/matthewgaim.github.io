b97e9fd81102b5b6ce9c7cd1ee09e0cb21c14d44
matthewgaim <matthewgaim@gmail.com>
2015-08-30 15:46:09 -0700

    Finished, i think.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-debugger
-//while(true){
-function checkTime(){
-  var time = new Date();
-  var hours = time.getHours();
-  var seconds = time.getSeconds();
-  var timeHTML = document.getElementById("time");
-  timeHTML.value="The time is " + hours.toString() + ":" + seconds.toString();
+function picClick(){
+	var sexy = new Audio("sexy.wav");
+	sexy.play();
 }
-//}
