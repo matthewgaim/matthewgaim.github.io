4fb8af68d8ca614ef56662f1712f2b0d426c5f1f
matthewgaim <matthewgaim@gmail.com>
2015-12-29 16:42:23 -0800

    Completely Remodeled
    -Hitmarker Sounds
    -Whole CSS & HTML Change
    -Integrated Typed.js
    -Spinny Social Media Buttons
    -"Made With Love"

---
+++ script/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".name").typed({
+    strings: ["","Who is he?",
+    "A High School programmer",
+    "Vice President of Hack Club SLHS",
+    "What does he know, You might be asking...",
+    "Javascript, Java(Android), HTML and CSS",
+    "*John Cena theme song starts playing*",
+    "AND HIS NAME IS MATTHEW GAIM!",],
+    typeSpeed: 15
+  });
+});
