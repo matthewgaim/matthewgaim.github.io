08cf2ab70aecdfadad857c2b12adc030a87ed1b5
Matthew Gaim <matthewgaim@gmail.com>
2016-06-02 21:47:33 -0700

    Website Remodel V4

--- script/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-     $(".name").typed({
-            strings: ["Matthew Gaim ^900", "Web Developer ^900", "Vice President of Hack Club SLHS ^900", "Hackathon Enthusiast ^900", "I am Matthew Gaim"],
-            typeSpeed: 100
-     });
-});
