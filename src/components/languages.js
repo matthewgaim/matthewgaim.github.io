6ef837df49440fb3f19712e4d1aa3df8dcc2cdad
Matthew Gaim <matthewgaim@gmail.com>
2020-08-06 22:21:29 -0700

    Styled 'areas' buttons
    - TODO: Make react page viewable on GitHub Pages

--- src/components/languages.js
+++ src/components/languages.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-
+import './languages.css';
 const languages = (props) => {
     // List of Web Development Languages
 
@@ -11,7 +11,7 @@ const languages = (props) => {
             image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png'
         },{
             language: 'React (Native)',
-            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
+            image: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
         }
     ].map((item)=> 
         <li style={{display: 'inline-block', margin: "1vw"}}>
@@ -23,10 +23,10 @@ const languages = (props) => {
     // List of Object Oriented Languages
     const oopList = [{
         language: 'Python',
-        image: 'https://www.python.org/static/opengraph-icon-200x200.png'
+        image: 'https://image.flaticon.com/icons/svg/1822/1822899.svg'
     },{
         language: 'Java',
-        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/1200px-Java_programming_language_logo.svg.png'
+        image: 'https://image.flaticon.com/icons/svg/226/226777.svg'
     },{
         language: '',
         image: ''
@@ -37,34 +37,50 @@ const languages = (props) => {
         </li>
     );
 
+    // List of DB languages
     const databaseList = [{
         language: 'MongoDB',
         image: 'https://cdn.iconscout.com/icon/free/png-512/mongodb-3-1175138.png'
     },{
-        language: 'SQL',
-        image: 'https://image.flaticon.com/icons/svg/29/29594.svg'
+        language: 'MySQL',
+        image: 'https://image.flaticon.com/icons/svg/1199/1199128.svg'
     },{
-        language: '',
-        image: ''
+        language: 'AWS',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg'
     }].map((item)=> 
-        <li style={{display: 'inline-block', margin: "1vw"}}>
+        <li style={{display: 'inline-block', margin: "1vw"}} key={item.language}>
             <img style={{height:'100px', width: '100px'}} src={item.image} alt={item.language}/>
             <p>{item.language}</p>
         </li>
     );
-    
+
+    const otherList = [{
+        language: 'Git',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg'
+    },{
+        language: 'PowerShell',
+        image: 'https://raw.githubusercontent.com/gist/Xainey/d5bde7d01dcbac51ac951810e94313aa/raw/6c858c46726541b48ddaaebab29c41c07a196394/PowerShell.svg'
+    }].map((item)=> 
+        <li style={{display: 'inline-block', margin: "1vw"}} key={item.language}>
+            <img style={{height:'100px', width: '100px'}} src={item.image} alt={item.language}/>
+            <p>{item.language}</p>
+        </li>
+    );
+
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [currentList, setCurrentList] = useState(webDevList);
+    const [currentList, setCurrentList] = useState(0);
+    const list = [webDevList, oopList, databaseList, otherList];
 
     return (
         <div >
             <h3>Knowledgable Areas</h3>
             <div>
-                <button onClick={()=>setCurrentList(webDevList)}>Web Development</button>
-                <button onClick={()=>setCurrentList(oopList)}>OOPs</button>
-                <button onClick={()=>setCurrentList(databaseList)}>Databases</button>
+                <a href="#" className={currentList === 0 ? "buttonActive" : "button"} onClick={()=>setCurrentList(0)}>Web Development</a>
+                <a href="#" className={currentList === 1 ? "buttonActive" : "button"} onClick={()=>setCurrentList(1)}>OOPs</a>
+                <a href="#" className={currentList === 2 ? "buttonActive" : "button"} onClick={()=>setCurrentList(2)}>Database</a>
+                <a href="#" className={currentList === 3 ? "buttonActive" : "button"} onClick={()=>setCurrentList(3)}>Tools</a>
             </div>
-            <ul style={{listStyle: "none"}}>{currentList}</ul>
+            <ul style={{listStyle: "none"}}>{list[currentList]}</ul>
         </div>
     );
 };
