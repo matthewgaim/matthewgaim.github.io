16d90c2f7fdf7ea9fa6d3bb0d0c8ae131c0e8f88
Matthew Gaim <matthewgaim@gmail.com>
2020-08-07 15:42:57 -0700

    Updates

---
+++ precache-manifest.b3bf65af2de7c0067d41070a65e55b67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f3224f196cdd4f4dec381d99071650",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83c97ecd680679b09805",
+    "url": "/static/css/main.6939150b.chunk.css"
+  },
+  {
+    "revision": "8e7425d6b0880446c852",
+    "url": "/static/js/2.641cc28a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.641cc28a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c97ecd680679b09805",
+    "url": "/static/js/main.e00ab937.chunk.js"
+  },
+  {
+    "revision": "90f7d4934ce631cde412",
+    "url": "/static/js/runtime-main.a1d39f49.js"
+  },
+  {
+    "revision": "a82990bb590888fd7df8ff903c71539e",
+    "url": "/static/media/Resume.a82990bb.pdf"
+  },
+  {
+    "revision": "adcd1bcba88a4a95677e56d21b6d7280",
+    "url": "/static/media/matthew.adcd1bcb.jpg"
+  }
+]);
