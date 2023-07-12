0e673217be61d84fda9377e339423b9e754caa2b
Matthew Gaim <matthewgaim@gmail.com>
2023-07-12 13:02:48 -0700

    Add contacts section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css';
 import Top from './components/Top/Top';
 import Experiences from './components/Experiences/Experiences';
-import Skills from './components/Skills/Skills';
 import Contact from './components/Contact/Contact';
 function App() {
   return (
     <div className="App">
       <Top />
       <Experiences />
-      <Skills />
       <Contact />
     </div>
   );
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,24 @@
 import './Contact.css';
+import github from '../../assets/img/github.png';
+import linkedin from '../../assets/img/linkedin.png';
+import email from '../../assets/img/email.png';
 
 function Contact() {
   return (
-    <>
-        
-    </>
+    <div className='pb-4 pt-4'>
+      <text class='findMeAt text-4xl font-semibold p-3 text-center font-serif'>Find me here:</text>
+      <div class='socials flex flex-wrap justify-center content-center'>
+          <a href="https://www.github.com/matthewgaim/" className='contact-link px-8'>
+              <img src={github} alt="GitHub" title="GitHub" />
+          </a>
+          <a href="https://www.linkedin.com/in/matthewgaim/" className='contact-link px-8'>
+              <img src={linkedin} alt="LinkedIn" title="LinkedIn" />
+          </a>
+          <a href="mailto:matthewgaim@gmail.com" className='contact-link px-8'>
+              <img src={email} alt="Email" title="Email" />
+          </a>
+      </div>
+    </div>
   );
 }
 
